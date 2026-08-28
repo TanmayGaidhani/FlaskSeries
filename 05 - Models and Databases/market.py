2a6d24f717efaa8f7669bd1cdbdf214312435407
TanmayGaidhani <vedant's email>
2026-08-28 19:45:27 +0530

    Fix incorrect SQLite database path in market app

--- 05 - Models and Databases/market.py
+++ 05 - Models and Databases/market.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+import os
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.root_path, 'market.db')
 db = SQLAlchemy(app)
 
 class Item(db.Model):
